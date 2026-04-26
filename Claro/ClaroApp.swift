5ad0bbf62d4d393f5ab5c4860637583104050e72
Ryan Curran <rcurran@mozilla.com>
2026-04-26 10:02:02 -0400

    Initial Commit

---
+++ Claro/ClaroApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaroApp.swift
+//  Claro
+//
+//  Created by Ryan Curran on 4/26/26.
+//
+
+import SwiftUI
+
+@main
+struct ClaroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Claro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Claro
+//
+//  Created by Ryan Curran on 4/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
